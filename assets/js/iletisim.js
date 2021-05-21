60d472314e9d14241777da3e129d22179482b60f
Ahsen Ceyda Altaş <64913741+AhsenAltas@users.noreply.github.com>
2021-05-21 23:13:34 +0300

    Düzenleme

--- assets/js/iletisim.js
+++ assets/js/iletisim.js
@@ -1,15 +1,3 @@
 function islemTemizle() {
-	document.getElementById("ad").value = "Adınızı yazınız";
-	document.getElementById("mail").value = "E-Mail giriniz";
-
-	document.getElementById("mesaj").value = "";
-
-	document.getElementById("erkek").checked = "";
-	document.getElementById("kadin").checked = "";
-	document.getElementById("diger").checked = "";
-
-	document.getElementById("gun").value = "gun";
-	document.getElementById("ay").value = "ay";
-	document.getElementById("yil").value = "yil";
-
+document.getElementById("formcuk").reset();
 }
